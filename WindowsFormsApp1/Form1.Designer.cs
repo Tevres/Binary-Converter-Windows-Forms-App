5ce37fe22a573e9765d74e27972565f2015f6313
Daniel <Daniel@DESKTOP-DL3Q94Q>
2024-11-03 17:19:32 +0100

    Projektdateien hinzufügen.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class BinaryConverter
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BinaryConverter));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNumberInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Klick mich";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxNumberInput
+            // 
+            this.textBoxNumberInput.BackColor = System.Drawing.Color.AliceBlue;
+            this.textBoxNumberInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNumberInput.Location = new System.Drawing.Point(12, 15);
+            this.textBoxNumberInput.Name = "textBoxNumberInput";
+            this.textBoxNumberInput.Size = new System.Drawing.Size(776, 31);
+            this.textBoxNumberInput.TabIndex = 2;
+            this.textBoxNumberInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBoxNumberInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxNumberInput_KeyPress);
+            // 
+            // BinaryConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(797, 973);
+            this.Controls.Add(this.textBoxNumberInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BinaryConverter";
+            this.Text = "BinaryConverter";
+            this.Load += new System.EventHandler(this.BinaryConverter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNumberInput;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class BinaryConverter : Form
+    {
+        public BinaryConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBoxNumberInput.Text, out int eingegebeneZahl);
+
+            // Initialisiere eine leere Zeichenfolge für die Binärzahl
+            string binaerZahl = "";
+
+            // Liste zur Speicherung der Schritte
+            List<string> schritte = new List<string>(); 
+            
+            // Konvertiere die eingegebene Zahl in die binäre Darstellung
+            while (eingegebeneZahl > 0)
+            {
+                int bit = eingegebeneZahl % 2;
+                int h = eingegebeneZahl / 2;
+                int rest = eingegebeneZahl - (h * 2);
+                
+                // Füge den aktuellen Schritt zur Liste hinzu
+                schritte.Add($"--> {eingegebeneZahl} / 2 = {h} Rest: {rest}");
+
+                // Füge das Bit zur Binärzahl hinzu
+                binaerZahl = bit.ToString() + binaerZahl;
+
+                // Aktualisiere die eingegebene Zahl
+                eingegebeneZahl = h; 
+            }
+
+            // Wenn die Eingabe 0 ist, muss die Binärdarstellung "0" sein
+            if (binaerZahl == "")
+            {
+                binaerZahl = "0";
+            }
+
+            // Gebe alle Schritte aus
+            label1.Text = "Binär: " + binaerZahl + "\n\nSchritte:\n" + string.Join("\n", schritte);
+        }
+    
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNumberInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BinaryConverter_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
